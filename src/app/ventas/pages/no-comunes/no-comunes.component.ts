0b822a9400a4549e1fbb9b3744fbdca481e86546
Alan Kralemann <akralemann@gmail.com>
2021-09-23 12:02:08 -0300

    Async Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -69,5 +70,16 @@ export class NoComunesComponent {
       }
     ]
 
+    // Async Pipe
+
+    miObservable = interval(5000);
+
+    valorPromesa = new Promise ( (resolve, reject) => {
+
+      setTimeout(() => {
+        resolve( 'Tenemos data de promesa' );
+      }, 3500);
+
+    });
 
 }
